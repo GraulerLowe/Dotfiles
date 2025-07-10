71eac3d510662e4f51738be2e6525f7f5dcb4f1d
GraulerLowe <gaeleon@ciencias.unam.mx>
2025-07-09 20:47:21 -0600

    Eliminacion de install y emacs

--- .emacs.d/elpa/night-owl-theme-20200622.1943/night-owl-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "night-owl-theme" "20200622.1943"
-  "A color theme for the night owls out there."
-  '((emacs "24"))
-  :url "http://github.com/aaronjensen/night-owl-theme"
-  :commit "50315d6a4e170dccc83bf2d59a8a761f5ea32bb6"
-  :revdesc "50315d6a4e17"
-  :authors '(("Aaron Jensen" . "aaronjensen@gmail.com"))
-  :maintainers '(("Aaron Jensen" . "aaronjensen@gmail.com")))
