56edfdaef5882426eb3446378a3d7ca147ce5d90
GraulerLowe <gaeleon@ciencias.unam.mx>
2025-08-12 15:06:04 -0600

    River actualizado

--- river.sh
+++ river.sh
@@ -8,7 +8,7 @@ install_dependencies() {
         sudo pacman --noconfirm --needed -S river fuzzel nemo foot github-cli waybar webp-pixbuf-loader mako brightnessctl pactl ttf-jetbrains-mono ttf-fira-code 
     elif [ -x "$(command -v dnf)" ]; then 
         echo -e "[*] Instalando paquetes usando dnf"
-        sudo dnf install -y hyprland rofi nemo kitty gh waybar emacs 
+        sudo dnf install -y river fuzzel nemo foot gh waybar emacs brightnessctl gedit mako grim
     else
         echo -e "[*] FALLO LA INSTALACION DEL PAQUETES: No se encontro el gestor de paquetes. Debes instalar los paquetes de forma manual." >&2
     fi
@@ -47,18 +47,47 @@ nerd_fonts_manuall (){
     echo "[*] Se han instalado las fuentes de texto correspodientes. Si es necesario reinicia el equipo"
 }
 
+emacs (){
+    clear
+    echo -e "1. Nobara Linux"
+    echo -e "2. Arch Linux"
+    echo -e "3. Salir"
+    read -p "[*] Elige la version de emacs que prefieras:" x
+    case $x in
+        [1]* ) emacs_nobara; exit;;
+        [2]* ) emacs_arch; exit;;
+        [3]* ) exit;;
+    esac
+}
+
+emacs_nobara() {
+    echo -e "[*] Se instala la configuracion de Nobara Linux"
+    mkdir $HOME/.emacs.d
+    cp -r emacs/emacs_nobara/init.el $HOME/.emacs.d/
+    echo -e "[*] Se instaló la configuracion de emacs"
+}
+
+emacs_arch() {
+    echo -e "[*] Se instala la configuracion de Nobara Linux"
+    mkdir $HOME/.emacs.d
+    cp -r emacs/emacs_arch/init.el $HOME/.emacs.d/
+    echo -e "[*] Se instaló la configuracion de emacs"
+}
+
 while true; do
     clear
     echo -e "Script para la instalacion del gestor de ventanas River"
     echo -e "[*] Elige la opcion:"
     echo -e "1. Instalar los paquetes."
     echo -e "2. Copiar los archivos."
-    echo -e "3. Exit."
+    echo -e "3. Instalar configuracion de emacs (solo si usas este editor)."
+    echo -e "4. Exit."
     read -p "[*] Your choice: " x
      
     case $x in
         [1]* ) install_dependencies; exit;;
         [2]* ) copy_directory; nerd_fonts_manuall; exit;;
-        [3]* ) exit;;
+        [3]* ) emacs; ;;
+        [4]* ) exit;;
     esac
 done
